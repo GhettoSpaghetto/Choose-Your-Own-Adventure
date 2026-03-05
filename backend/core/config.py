2063c58830c87ad3e55d62193a2964d359c89e17
Samarth Kaushal <samarth.chikki.142@gmail.com>
2026-03-06 04:51:44 +0530

    fix frontend  error when creating a story

---
+++ backend/core/config.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
+from pydantic import field_validator
+
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    API_PREFIX: str = "/api"
+    DEBUG: bool = False
+
+
+    ALLOWED_ORIGINS: str= "" 
+    GOOGLE_API_KEY: str
+
+    @field_validator("ALLOWED_ORIGINS")
+    def parse_allowed_origins(cls, v: str) -> List[str]:
+        return v.split(",") if v else []
+    
+    model_config = SettingsConfigDict(
+        env_file= ".env",
+        env_file_encoding= "utf-8",
+        case_sensitive= True
+    )
+
+
+settings = Settings()
